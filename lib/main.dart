de92f23a640c953b1e52b8014470de7b09151bc9
Danny Francisco <dcfran@gmail.com>
2024-12-05 23:47:20 +0800

    Refactor main app structure and update home page title

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,3 @@ import 'package:flutter_petclinic/petclinic_app.dart';
 void main() {
   runApp(const PetClinicApp());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-    );
-  }
-}
