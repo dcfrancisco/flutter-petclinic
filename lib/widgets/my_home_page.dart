e8237ef4e7722a50de5479e2d24af4c1cf3b4ce5
danny.c.francisco <danny.c.francisco@accenture.com>
2024-12-05 13:35:16 +0800

    add bottom navigation bar

--- lib/widgets/my_home_page.dart
+++ lib/widgets/my_home_page.dart
@@ -10,7 +10,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  static final List<Widget> _widgetOptions = <Widget>[Text('')];
+  static final List<Widget> _widgetOptions = <Widget>[
+    Center(child: Text('Home Screen')),
+    Center(child: Text('Find Orner')),
+    Center(child: Text('Veterinarian')),
+  ];
+
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,9 +31,20 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(child: _widgetOptions.first),
+      body: _widgetOptions.elementAt(_selectedIndex),
 
-      bottomNavigationBar: BottomNavigationBar(items: []),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Owner'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Veterinarian',
+          ),
+        ],
+      ),
     );
   }
 }
